79ad645ed84e5d984f227a07b32076e6746cb973
Hossein ZAER <hzaer@yahoo.com>
2025-01-12 21:00:25 +0100

    calendar shows event. need to create 2 seperate pages for updating/creating an event

--- src/components/Calendrier/Calendrier.js
+++ src/components/Calendrier/Calendrier.js
@@ -1,45 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-// importer CSS
-import '@schedule-x/theme-default/dist/index.css'
+import '../../css/Calendar/Calendar.css';
 
 
-import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/react'
-import {
-  createViewDay,
-  createViewMonthAgenda,
-  createViewMonthGrid,
-  createViewWeek,
-} from '@schedule-x/calendar'
-import { createEventsServicePlugin } from '@schedule-x/events-service'
- 
-import '@schedule-x/theme-default/dist/index.css'
- 
+// import '@schedule-x/theme-shadcn/dist/index.css' //theme : black
+// on peut définir des couleurs differentes pour chaque matière, mais il faut en créer une pour chaqu'une
+
+import { ScheduleXCalendar,useCalendarApp } from '@schedule-x/react';
+import { createViewWeek, createViewMonthGrid, createViewMonthAgenda } from '@schedule-x/calendar';
+import { createEventsServicePlugin } from '@schedule-x/events-service';
+import { createEventModalPlugin } from '@schedule-x/event-modal';
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop';
+import { createViewDay } from '@schedule-x/calendar';
+import '@schedule-x/theme-default/dist/calendar.css'
+import { createScrollControllerPlugin } from '@schedule-x/scroll-controller'
+import { createCurrentTimePlugin } from '@schedule-x/current-time'
+
+
 function Calendar() {
-  const eventsService = useState(() => createEventsServicePlugin())[0]
- 
+
   const calendar = useCalendarApp({
-    views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
+    views: [createViewDay(), createViewMonthAgenda(),createViewWeek(), createViewMonthGrid()],
     events: [
       {
         id: '1',
         title: 'Event 1',
-        start: '2023-12-16',
-        end: '2023-12-16',
+        start: '2025-01-13 10:22',
+        end: '2025-01-13 12:22',
+        description:"Pourquoi t'a clické sur cet événement Piravine?",
+        people:["Hossein","Romain sans Thomas","Ronon!","Pire"]
       },
     ],
-    plugins: [eventsService]
+    plugins: [
+      createEventsServicePlugin(),
+      createEventModalPlugin(),
+      createDragAndDropPlugin(),
+      createScrollControllerPlugin({initialScroll : '08:00'}),
+      createCurrentTimePlugin({fullWeekWidth: true})
+    ],
+    // theme: 'shadcn' theme : black
   })
- 
-  useEffect(() => {
-    // get all events
-    eventsService.getAll()
-  }, [])
- 
+
   return (
-    <div>
+      <div>
       <ScheduleXCalendar calendarApp={calendar} />
-    </div>
+      </div>
   )
 }
  
