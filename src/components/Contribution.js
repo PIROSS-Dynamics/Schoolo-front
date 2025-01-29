71883d4c7a22bd68f40b946477563bf5da93b041
Ronan <ronan.besn@gmaill.com>
2025-01-29 11:41:18 +0100

    graph with zoom

--- src/components/Contribution.js
+++ src/components/Contribution.js
@@ -18,40 +18,44 @@ const Contribution = () => {
   useEffect(() => {
     if (!data) return; // wait the loading
 
+    
     const width = 780;
     const height = 780;
-
-    // color of the graphic
-    const color = d3.scaleLinear()
-      .domain([0, 4])
-      .range(["#98e6cc", "#00a5e7"])
-      .interpolate(d3.interpolateHcl);
-
-      
-    const pack = d3.pack()
-      .size([width, height])
-      .padding(5);
 
     // transform json to graph
     const root = d3.hierarchy(data)
       .sum(d => d.value || 1) 
       .sort((a, b) => b.value - a.value);
 
-    pack(root);
 
+    const pack = d3.pack()
+      .size([width, height])
+      .padding(5);
+
+    
     setFocus(root);
+    pack(root);
+    
+     // color of the graphic
+    const color = d3.scaleLinear()
+      .domain([0, 4])
+      .range(["#98e6cc", "#00a5e7"])
+      .interpolate(d3.interpolateHcl);
 
     // Sélectionner et créer le SVG
     const svg = d3.select(ref.current)
       .attr("viewBox", `0 0 ${width} ${height}`)
-      .attr("width", width)
-      .attr("height", height)
-      .style("max-width", "100%")
-      .style("height", "auto")
+      .attr("width", "100%") 
+      .attr("height", "100%")
+    
       .style("display", "block")
       .style("cursor", "pointer");
 
-    const g = svg.append("g");
+
+    svg.selectAll("*").remove();
+
+    const g = svg.append("g").attr("transform", `scale(1)`);
+
     // Dessiner les cercles
     g.selectAll("circle")
       .data(root.descendants())
@@ -62,6 +66,8 @@ const Contribution = () => {
       .attr("transform", d => `translate(${d.x},${d.y})`)
       .attr("r", d => d.r)
       .on("click", (event, d) => zoom(event, d));
+      
+      
 
     // Ajouter les labels des nœuds
     g.selectAll("text")
@@ -72,23 +78,51 @@ const Contribution = () => {
       .attr("font-size", d => (d.children ? 14 : 10))
       .attr("fill", "black")
       
-      
       .text(d => d.data.name);
 
   }, [data]); // Re-exécuter lorsqu'on a les données
 
+  
   const zoom = (event, d) => {
-    // Mettre à jour le focus sur le nœud cliqué
-    setFocus(d);
+    
+    setFocus(d); 
 
-    alert(d.data.name);
-  };
+    //smooth animation
+    const transition = d3.select(ref.current)
+    .transition()
+    .duration(750);
+
+    const zoomFactor = 0.8;
+
+    
+    var k;
+    //we don't apply zoomFactor for root
+    if (d.data.name === "Root"){
+      k = 1;
+    } else {
+      var k = (800 / (d.r * 2)) * zoomFactor; 
+    }
+
+
+    
+    let x = 800 / 2 - d.x * k; // horizontaly move
+    let y = 800 / 2 - d.y * k; // vertivaly move
+
+    let g = d3.select(ref.current).select("g");
+
+    g.transition(transition)
+      .attr("transform", `translate(${x},${y}) scale(${k})`);
+    };
+
   
   return (
     
     <div>
       <h1> Contribution in the team </h1>
-      <svg ref={ref}></svg>
+
+      <div className="graph-container">
+        <svg ref={ref}></svg>
+      </div>
     </div>
     
   );
